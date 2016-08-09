ba796cb84ca15de19443de7e8fce868294f93dd5
Andreas Rolén <andreasrolen93@gmail.com>
2016-08-09 18:47:06 +0200

    design for the app

--- android/AutoDJ/app/src/main/java/com/hackoutwest/adderollen/autodj/Activities/Activities/CounterActivity.java
+++ android/AutoDJ/app/src/main/java/com/hackoutwest/adderollen/autodj/Activities/Activities/CounterActivity.java
@@ -1,14 +1,17 @@
 package com.hackoutwest.adderollen.autodj.Activities.Activities;
 
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.RadioButton;
-import android.widget.RadioGroup;
 import android.widget.TextView;
 
-import com.hackoutwest.adderollen.autodj.Activities.AsyncTasks.AsyncSendClick;
 import com.hackoutwest.adderollen.autodj.Activities.Constants;
 import com.hackoutwest.adderollen.autodj.R;
 
@@ -17,35 +20,36 @@ import java.util.Map;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnCheckedChanged;
 import butterknife.OnClick;
 
 public class CounterActivity extends AppCompatActivity {
 
     private static final String TAG = "CounterActivity";
 
-    @Bind(R.id.gender_radiogroup)
-    RadioGroup genderRadioGroup;
+    @Bind(R.id.gender_container)
+    LinearLayout genderContainer;
 
-    @Bind(R.id.male_radiobutton)
-    RadioButton maleRadioButton;
+    @Bind(R.id.gender_male_button)
+    ImageButton maleButton;
 
-    @Bind(R.id.female_radiobutton)
-    RadioButton femaleRadioButton;
+    @Bind(R.id.gender_female_button)
+    ImageButton femaleButton;
 
-    @Bind(R.id.age_radiogroup)
-    RadioGroup ageRadioGroup;
+    @Bind(R.id.age_1_checkbox)
+    CheckBox ageOneCheckbox;
 
-    @Bind(R.id.age_1_radiobutton)
-    RadioButton ageOneRadioButton;
+    @Bind(R.id.age_2_checkbox)
+    CheckBox ageTwoCheckbox;
 
-    @Bind(R.id.age_2_radiobutton)
-    RadioButton ageTwoRadioButton;
+    @Bind(R.id.age_3_checkbox)
+    CheckBox ageThreeCheckbox;
 
-    @Bind(R.id.age_3_radiobutton)
-    RadioButton ageThreeRadioButton;
+    @Bind(R.id.age_4_checkbox)
+    CheckBox ageFourCheckbox;
 
-    @Bind(R.id.age_4_radiobutton)
-    RadioButton ageFourRadioButton;
+    @Bind(R.id.age_5_checkbox)
+    CheckBox ageFiveCheckbox;
 
     @Bind(R.id.inc_button)
     Button incButton;
@@ -55,6 +59,11 @@ public class CounterActivity extends AppCompatActivity {
 
     @Bind(R.id.current_guest_amount_textview)
     TextView currentGuestAmountTextview;
+
+    private boolean isMale = true;
+    private CheckBox[] checkBoxes;
+    private boolean maleIsActive;
+    private boolean femaleIsActive;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,6 +76,15 @@ public class CounterActivity extends AppCompatActivity {
             decButton.setEnabled(false);
         }
 
+        maleIsActive = true;
+
+        checkBoxes = new CheckBox[5];
+        checkBoxes[0] = ageOneCheckbox;
+        checkBoxes[1] = ageTwoCheckbox;
+        checkBoxes[2] = ageThreeCheckbox;
+        checkBoxes[3] = ageFourCheckbox;
+        checkBoxes[4] = ageFiveCheckbox;
+
     }
 
     @OnClick(R.id.inc_button)
@@ -77,6 +95,70 @@ public class CounterActivity extends AppCompatActivity {
     @OnClick(R.id.dec_button)
     public void decClick() {
         sendClick(Constants.DEC);
+    }
+
+
+    @OnClick(R.id.gender_male_button)
+    public void maleClick() {
+        if (!maleIsActive) {
+            Drawable genderDrawable = getDrawable(R.drawable.sel_radiobutton_male) ;
+
+            maleIsActive = true;
+            femaleIsActive = false;
+            femaleButton.setBackgroundColor(ContextCompat.getColor(this,R.color.white));
+            maleButton.setBackgroundColor(ContextCompat.getColor(this,R.color.male));
+
+            for (int i = 0; i < checkBoxes.length; i++) {
+                if (checkBoxes[i].isChecked()) {
+                    checkBoxes[i].setBackground(genderDrawable);
+                }
+            }
+        }
+
+
+    }
+
+    @OnClick(R.id.gender_female_button)
+    public void femaleClick() {
+        if (!femaleIsActive) {
+            Drawable genderDrawable = getDrawable(R.drawable.sel_radiobutton_female);
+
+            femaleIsActive = true;
+            maleIsActive = false;
+            femaleButton.setBackgroundColor(ContextCompat.getColor(this,R.color.female));
+            maleButton.setBackgroundColor(ContextCompat.getColor(this,R.color.white));
+
+            for (int i = 0; i < checkBoxes.length; i++) {
+                if (checkBoxes[i].isChecked()) {
+                    checkBoxes[i].setBackground(genderDrawable);
+                }
+            }
+        }
+
+
+    }
+
+    private int checkedBox;
+
+    @OnCheckedChanged({R.id.age_1_checkbox, R.id.age_2_checkbox, R.id.age_3_checkbox, R.id.age_4_checkbox, R.id.age_5_checkbox})
+    public void checkboxCheckedChanged(CheckBox checkBox, boolean isChecked) {
+        if (isChecked) {
+            checkedBox = checkBox.getId();
+            Drawable genderDrawable = isMale ? getDrawable(R.drawable.sel_radiobutton_male) : getDrawable(R.drawable.sel_radiobutton_female);
+            checkBox.setBackground(genderDrawable);
+
+            for (int i = 0; i < checkBoxes.length; i++) {
+                if (checkBoxes[i].getId() != checkBox.getId()) {
+                    checkBoxes[i].setChecked(false);
+                }
+            }
+
+        } else {
+            Log.d(TAG,"asdf");
+            if (checkBox.getId() == checkedBox) {
+                checkBox.setChecked(true);
+            }
+        }
     }
 
 
@@ -104,13 +186,13 @@ public class CounterActivity extends AppCompatActivity {
 
     private void sendClick(int value) {
         updateGuestNumber(value);
-        int genderRadioButtonId = genderRadioGroup.getCheckedRadioButtonId();
-        int ageRadioButtonId = ageRadioGroup.getCheckedRadioButtonId();
+        int genderRadioButtonId = getCheckedGenderId();
+        int ageCheckboxId = getCheckedCheckboxId();
+        String gender;
         String ageGroup;
-        String gender = "";
         String incDec;
 
-        switch (ageRadioButtonId) {
+        switch (ageCheckboxId) {
             case Constants.AGE_GROUP_1:
                 ageGroup = Constants.AGE_GROUP_1_STRING;
                 break;
@@ -159,8 +241,29 @@ public class CounterActivity extends AppCompatActivity {
         parameters.put(Constants.GENDER, gender);
         parameters.put(Constants.VALUE, incDec);
 
+        Log.d(TAG, "AGE: " + ageGroup);
+        Log.d(TAG, "GENDER: " + gender);
+        Log.d(TAG, "VALUE: " + incDec);
+
         //new AsyncSendClick(this).execute(parameters);
 
+    }
+
+    private int getCheckedGenderId() {
+        if (maleIsActive) {
+            return R.id.gender_male_button;
+        } else {
+            return R.id.gender_female_button;
+        }
+    }
+
+    private int getCheckedCheckboxId() {
+        for (int i = 0; i < checkBoxes.length; i++) {
+            if (checkBoxes[i].isChecked()) {
+                return checkBoxes[i].getId();
+            }
+        }
+        return -1;
     }
 
 
--- android/AutoDJ/app/src/main/java/com/hackoutwest/adderollen/autodj/Activities/Constants.java
+++ android/AutoDJ/app/src/main/java/com/hackoutwest/adderollen/autodj/Activities/Constants.java
@@ -2,8 +2,6 @@ package com.hackoutwest.adderollen.autodj.Activities;
 
 import com.hackoutwest.adderollen.autodj.R;
 
-import java.security.PublicKey;
-
 /**
  * Created by adderollen on 2016-08-09.
  */
@@ -11,21 +9,21 @@ public class Constants {
 
 
     public static final String AGE = "AGE";
-    public static final int AGE_GROUP_1 = R.id.age_1_radiobutton;
+    public static final int AGE_GROUP_1 = R.id.age_1_checkbox;
     public static final String AGE_GROUP_1_STRING = "AGE_GROUP_1";
-    public static final int AGE_GROUP_2 = R.id.age_2_radiobutton;
+    public static final int AGE_GROUP_2 = R.id.age_2_checkbox;
     public static final String AGE_GROUP_2_STRING = "AGE_GROUP_2";
-    public static final int AGE_GROUP_3 = R.id.age_3_radiobutton;
+    public static final int AGE_GROUP_3 = R.id.age_3_checkbox;
     public static final String AGE_GROUP_3_STRING = "AGE_GROUP_3";
-    public static final int AGE_GROUP_4 = R.id.age_4_radiobutton;
+    public static final int AGE_GROUP_4 = R.id.age_4_checkbox;
     public static final String AGE_GROUP_4_STRING = "AGE_GROUP_4";
-    public static final int AGE_GROUP_5 = R.id.age_5_radiobutton;
+    public static final int AGE_GROUP_5 = R.id.age_5_checkbox;
     public static final String AGE_GROUP_5_STRING = "AGE_GROUP_5";
 
     public static final String GENDER = "GENDER";
-    public static final int GENDER_MALE = R.id.male_radiobutton;
+    public static final int GENDER_MALE = R.id.gender_male_button;
     public static final String GENDER_MALE_STRING = "GENDER_MALE";
-    public static final int GENDER_FEMALE = R.id.female_radiobutton;
+    public static final int GENDER_FEMALE = R.id.gender_female_button;
     public static final String GENDER_FEMALE_STRING = "GENDER_FEMALE";
 
     public static final String VALUE = "VALUE";

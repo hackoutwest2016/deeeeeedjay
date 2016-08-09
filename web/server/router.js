710b4863e07352e909b1f528fd3183594c50f381
Robin Andersson <k.robin.andersson@gmail.com>
2016-08-09 14:30:47 +0200

    add POST route to /guestUpdate

---
+++ web/server/router.js
@@ -0,0 +1,33 @@
+import { GuestUpdates } from '../imports/api/guestUpdates/collection.js';
+
+JsonRoutes.add('post', '/guestUpdate', (req, res, next) => {
+  const { age, gender, value } = req.body;
+  const timestamp = new Date(+req.body.timestamp);
+
+  let options = {
+    data: {
+      message: 'Success! guestUpdate has been registered',
+    },
+    code: 200,
+  };
+
+  GuestUpdates.insert(
+    { age,
+      gender,
+      value,
+      timestamp,
+    }, (err, guestUpdateId) => {
+      if (err) {
+        console.log(`Error on POST to guestUpdate: ${err.message}`);
+        options = {
+          data: {
+            message: err.message,
+          },
+          code: 500,
+        };
+      } else {
+        console.log('Success! guestUpdate has been registered');
+      }
+  });
+  return JsonRoutes.sendResult(res, options);
+});

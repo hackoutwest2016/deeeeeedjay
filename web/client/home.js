3c5324f28fcb8ef11d7db492210001da6df83fcf
Robin Andersson <k.robin.andersson@gmail.com>
2016-08-09 16:33:56 +0200

    add function for calculating 'current amounts' based on field values

--- web/client/home.js
+++ web/client/home.js
@@ -4,3 +4,12 @@ Template.home.onCreated(function() {
   });
 });
 
+function getFieldAmount(array, fun) {
+  let currentAmount = 0;
+  _.each(array, (item) => {
+    const amount = item.value === 'INC' ? 1 : -1;
+    currentAmount += fun(item) ? amount : 0;
+  });
+
+  return currentAmount;
+}

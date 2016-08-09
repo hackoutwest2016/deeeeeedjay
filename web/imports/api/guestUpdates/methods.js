212c439a759ba870fda0c64e76bb56a673c2322c
Robin Andersson <k.robin.andersson@gmail.com>
2016-08-09 14:29:33 +0200

    add guestUpdates/collection.js and guestUpdates/methods.js skeleton files

---
+++ web/imports/api/guestUpdates/methods.js
@@ -0,0 +1,20 @@
+if (Meteor.isServer) {
+  // This code only runs on the server
+
+  // e.g.:
+  // Meteor.publish('tasks', function tasksPublication() {
+  //  return Tasks.find();
+  // });
+}
+
+Meteor.methods({
+  // e.g.:
+
+  // 'tasks.insert' (text) {
+  //
+  // },
+
+  // 'tasks.remove' (taskId) {
+  //
+  // }
+});

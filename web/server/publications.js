e4d6b3b297331e2b15e7a44d515832fc15f78eee
Robin Andersson <k.robin.andersson@gmail.com>
2016-08-09 19:19:56 +0200

    add spotify authentication

--- web/server/publications.js
+++ web/server/publications.js
@@ -1,5 +1,14 @@
 Meteor.startup(() => {
-  
+  ServiceConfiguration.configurations.update(
+  { "service": "spotify" },
+  {
+    $set: {
+      "clientId": "403ae8720ffb4073b04f4a9984181dab",
+      "secret": "4dc1f953232a43d6a30b4a9b407c33a3"
+    }
+  },
+  { upsert: true }
+);
 });
 
 Meteor.publish('GuestUpdates.all', () => {
